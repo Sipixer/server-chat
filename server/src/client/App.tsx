f2a5044ca654207d6488045906c9c0702b9ef3c9
Sylvain Rougie <sipixer@gmail.com>
2025-10-09 09:22:22 +0000

    fix: change color

--- server/src/client/App.tsx
+++ server/src/client/App.tsx
@@ -4,45 +4,45 @@ import { ChatPage } from "./components/ChatPage";
 import { UsernameForm } from "./components/UsernameForm";
 
 const App = () => {
-	const [username, setUsername] = useState("");
-	const [isUsernameSet, setIsUsernameSet] = useState(false);
-	const [serverId, setServerId] = useState("");
+  const [username, setUsername] = useState("");
+  const [isUsernameSet, setIsUsernameSet] = useState(false);
+  const [serverId, setServerId] = useState("");
 
-	useEffect(() => {
-		fetch("/api/server-id")
-			.then((res) => res.json())
-			.then((data) => setServerId(data.id))
-			.catch((err) => console.error("Error fetching server ID:", err));
-	}, []);
+  useEffect(() => {
+    fetch("/api/server-id")
+      .then((res) => res.json())
+      .then((data) => setServerId(data.id))
+      .catch((err) => console.error("Error fetching server ID:", err));
+  }, []);
 
-	const handleUsernameSubmit = (e: React.FormEvent) => {
-		e.preventDefault();
-		if (username.trim()) {
-			setIsUsernameSet(true);
-		}
-	};
+  const handleUsernameSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (username.trim()) {
+      setIsUsernameSet(true);
+    }
+  };
 
-	// biome-ignore lint/correctness/noConstantCondition:  TESTING
-	const newColor = true
-		? "bg-gradient-to-br from-blue-500 to-emerald-500"
-		: "bg-gradient-to-br from-red-500 to-purple-500";
+  // biome-ignore lint/correctness/noConstantCondition:  TESTING
+  const newColor = false
+    ? "bg-gradient-to-br from-blue-500 to-emerald-500"
+    : "bg-gradient-to-br from-red-500 to-purple-500";
 
-	return (
-		<div
-			className={` overflow-hidden  flex flex-col w-screen h-[100dvh] ${newColor}`}
-		>
-			{!isUsernameSet ? (
-				<UsernameForm
-					username={username}
-					serverId={serverId}
-					onUsernameChange={setUsername}
-					onSubmit={handleUsernameSubmit}
-				/>
-			) : (
-				<ChatPage username={username} serverId={serverId} />
-			)}
-		</div>
-	);
+  return (
+    <div
+      className={` overflow-hidden  flex flex-col w-screen h-[100dvh] ${newColor}`}
+    >
+      {!isUsernameSet ? (
+        <UsernameForm
+          username={username}
+          serverId={serverId}
+          onUsernameChange={setUsername}
+          onSubmit={handleUsernameSubmit}
+        />
+      ) : (
+        <ChatPage username={username} serverId={serverId} />
+      )}
+    </div>
+  );
 };
 
 export default App;
